bacc7106c0fc6bc40e895fce07c246953139fba0
Patrick Hammond <patrick@madebyatomicrobot.com>
2016-06-25 00:20:08 -0400

    Replacing the existing proposition with one tailored to this event.

--- app/metadata/sitedata.js
+++ app/metadata/sitedata.js
@@ -30,5 +30,5 @@ module.exports = {
   socialFeed: {
     source: '/data/tweets.json'
   },
-  partnershipProposition: '/assets/GDG_DevFest_Ukraine_2015_Partnership_Proposition.pdf'
+  partnershipProposition: '/assets/Sponsor.pdf'
 };
