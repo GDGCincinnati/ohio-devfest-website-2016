1b0fedacc037b91f607523bc3a3a8da70423f336
Patrick Hammond <patrick@madebyatomicrobot.com>
2016-06-26 12:09:47 -0400

    Setting up the mail chimp integration.

--- app/metadata/config.js
+++ app/metadata/config.js
@@ -11,8 +11,8 @@ module.exports = {
     mapsApiKey: 'AIzaSyDNFE5RChWZGz2uq6CTtLEOtzD4fTSjPoI',
     // Mailchimp subscribe
     mailchimp: {
-        url: 'YOUR_MAILCHIMP_URL',
-        name: 'YOUR_MAILCHIMP_NAME'
+        url: '//ohiodevfest.us13.list-manage.com/subscribe/post?u=edb11ee7c2cb83eb0e7541e4d&amp;id=a60a395086',
+        name: 'b_edb11ee7c2cb83eb0e7541e4d_a60a395086'
     },
     disqusShortName: 'hoverboard-gdg-x'
 };
