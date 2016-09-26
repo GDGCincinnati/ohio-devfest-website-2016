563ca4f61481f1e13fe3448076df8252009e5ff9
Patrick Hammond <patrick@madebyatomicrobot.com>
2016-09-25 22:31:44 -0400

    Updating the early bird ticket sales as ended.

--- app/metadata/sitedata.js
+++ app/metadata/sitedata.js
@@ -19,22 +19,26 @@ module.exports = {
         name: 'Professional Early Bird',
         price: 50,
         currency: 'USD',
-        info: 'Ends on Sept 16, 2016'
+        info: 'Ends on Sept 16, 2016',
+        ended: true
       },{
         name: 'Student Early Bird',
         price: 25,
         currency: 'USD',
-        info: 'Ends on Sept 16, 2016'
+        info: 'Ends on Sept 16, 2016',
+        ended: true
       },{
         name: 'Professional',
         price: 65,
         currency: 'USD',
-        info: ''
+        info: '',
+        ended: false
       },{
         name: 'Student',
         price: 35,
         currency: 'USD',
-        info: ''
+        info: '',
+        ended: false
       }
     ],
     details: 'Tickets grant access to all conference sessions, coffee breaks, and lunch. Accommodation is NOT included in the ticket price.'
